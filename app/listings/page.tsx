0419646d8983f61c34693cfa35086177cbe72c4f
Adrien <147277135+amisplace@users.noreply.github.com>
2023-10-27 17:25:14 -0400

    Fix listing page auth flow

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -4,11 +4,27 @@ import Footer from "../../src/components/homepage/Footer";
 import ListingsBody from "../../src/components/listings/ListingsBody";
 import ListingsHeader from "../../src/components/listings/ListingsHeader";
 import Navbar from "../../src/components/common/Navbar";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
 
-const Listings = () => {
+const Listings = async () => {
+    const cookieStore = cookies();
+    const supabase = createServerComponentClient({
+        cookies: () => cookieStore,
+    });
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+        redirect("/login");
+    }
+    // This is where we can fetch our listings!
     return (
         <>
-            <Navbar />
+            <Navbar session={null} />
             <ListingsHeader />
             <ListingsBody />
             <Footer />
