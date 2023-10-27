a697f6bc076c0d64667144649ccd0e695c1deb2d
Adrien <147277135+amisplace@users.noreply.github.com>
2023-10-27 17:21:49 -0400

    Redirect to account page when logged in

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -3,11 +3,26 @@ import React from "react";
 import Footer from "../../src/components/homepage/Footer";
 import JoinBody from "../../src/components/join/JoinBody";
 import Navbar from "../../src/components/common/Navbar";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+    const cookieStore = cookies();
+    const supabase = createServerComponentClient({
+        cookies: () => cookieStore,
+    });
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+        redirect("/account");
+    }
     return (
         <>
-            <Navbar />
+            <Navbar session={null} />
             <JoinBody />
             <Footer requiresPadding={false} />
         </>
