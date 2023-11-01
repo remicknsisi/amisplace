e0b9b465e3e2bf696aa9602194ade931821a070e
Adrien <147277135+amisplace@users.noreply.github.com>
2023-11-01 00:20:17 -0400

    Fix missing Navbar param

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,22 @@ import React from "react";
 import AboutBody from "../../src/components/about/AboutBody";
 import Navbar from "../../src/components/common/Navbar";
 import Footer from "../../src/components/homepage/Footer";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
-const AboutPage = () => {
+const AboutPage = async () => {
+    const cookieStore = cookies();
+    const supabase = createServerComponentClient({
+        cookies: () => cookieStore,
+    });
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
     return (
         <>
-            <Navbar />
+            <Navbar session={session} />
             <AboutBody />
             <Footer requiresPadding={false} />
         </>
